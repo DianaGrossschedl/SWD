d9d29fe8e6e575780af5c05e10b822f0f22e1e83
Diana <51800713@edu.campus02.at>
2022-05-20 14:45:05 +0200

    Methoden drawElements und drawOneElement in FactoryCreateTile ergänzt

--- core/src/main/java/at/compus02/swd/ss2022/game/Main.java
+++ core/src/main/java/at/compus02/swd/ss2022/game/Main.java
@@ -6,9 +6,7 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
@@ -33,9 +31,14 @@ public class Main extends ApplicationAdapter {
 
 		FactoryCreateTile factory = new FactoryCreateTile();
 		factory.calculateBackground(gameObjects);
-		factory.calculateSign(gameObjects);
+		factory.setPlayer("Dog", gameObjects);
 
-		//gameObjects.add(new Tile_water());
+
+		factory.drawOneElement("Stone",gameObjects,1,1);
+		factory.drawOneElement("Stone",gameObjects,14,3);
+
+
+		factory.drawElements("Water", gameObjects);
 
 		font = new BitmapFont();
 		font.setColor(Color.WHITE);
--- core/src/main/java/at/compus02/swd/ss2022/game/gameobjects/FactoryCreateTile.java
+++ core/src/main/java/at/compus02/swd/ss2022/game/gameobjects/FactoryCreateTile.java
@@ -4,11 +4,13 @@ import com.badlogic.gdx.utils.Array;
 
 public class FactoryCreateTile {
 
-    public GameObject createStartingTiles(String tile){
+    public GameObject getTile(String tile){
 
         switch(tile) {
             case "Sign":
                 return new Sign();
+            case "Stone":
+                return new Stone();
             case "Bush":
                 return new Bush();
             case "Gras":
@@ -31,13 +33,15 @@ public class FactoryCreateTile {
     public Array<GameObject> calculateBackground(Array<GameObject> gameObjects){
         float x = -240;
         float y = 210;
+        String tile = "Gras";
 
         int countTile = 0;
 
         for(int j = 0; j < 16; j++) {
 
             for (int i = 0; i < 16; i++) {
-                gameObjects.add(createStartingTiles("Gras"));
+
+                gameObjects.add(getTile(tile));
                 gameObjects.get(countTile).setPosition(x, y);
                 x += 30;
                 countTile+=1;
@@ -45,25 +49,124 @@ public class FactoryCreateTile {
             y -= 30;
             x = -240;
         }
-
         return gameObjects;
-
-
     }
 
-    public Array<GameObject> calculateSign(Array<GameObject> gameObjects){
-        float x = 0;
-        float y = 0;
-
-                gameObjects.add(createStartingTiles("Dog"));
+    public Array<GameObject> setPlayer(String player, Array<GameObject> gameObjects){
+        float x = -15;
+        float y = -15;
+        //Array<GameObject> gameObjects = new Array<>();
+                gameObjects.add(getTile(player));
                 gameObjects.get(gameObjects.size-1).setPosition(x, y);
 
         return gameObjects;
+    }
 
+    public void drawOneElement(String tile, Array<GameObject> gameObjects, int xCoordinate, int yCoordinate){
+
+        gameObjects.add(getTile(tile));
+        gameObjects.get(gameObjects.size-1).setPosition(calculatePosition(xCoordinate),calculatePosition(yCoordinate));
 
     }
 
+    public Array<GameObject> drawElements(String tile, Array<GameObject> gameObjects){
 
+        Integer[][] positionWater = new Integer[25][2];
+                positionWater[0][0]=0;
+                positionWater[0][1]=3;
+
+                positionWater[1][0]=1;
+                positionWater[1][1]=3;
+
+                positionWater[2][0]=2;
+                positionWater[2][1]=3;
+
+                positionWater[3][0]=3;
+                positionWater[3][1]=3;
+
+                positionWater[4][0]=4;
+                positionWater[4][1]=3;
+
+                positionWater[5][0]=5;
+                positionWater[5][1]=3;
+
+                positionWater[6][0]=6;
+                positionWater[6][1]=3;
+
+                positionWater[7][0]=7;
+                positionWater[7][1]=3;
+
+                positionWater[8][0]=8;
+                positionWater[8][1]=3;
+
+                positionWater[9][0]=9;
+                positionWater[9][1]=3;
+
+                positionWater[10][0]=9;
+                positionWater[10][1]=4;
+
+                positionWater[11][0]=9;
+                positionWater[11][1]=5;
+
+                positionWater[12][0]=9;
+                positionWater[12][1]=6;
+
+                positionWater[13][0]=9;
+                positionWater[13][1]=7;
+
+                positionWater[14][0]=9;
+                positionWater[14][1]=8;
+
+                positionWater[15][0]=9;
+                positionWater[15][1]=9;
+
+                positionWater[16][0]=9;
+                positionWater[16][1]=10;
+
+                positionWater[17][0]=9;
+                positionWater[17][1]=11;
+
+                positionWater[18][0]=8;
+                positionWater[18][1]=11;
+
+                positionWater[19][0]=7;
+                positionWater[19][1]=11;
+
+                positionWater[20][0]=6;
+                positionWater[20][1]=11;
+
+                positionWater[21][0]=6;
+                positionWater[21][1]=12;
+
+                positionWater[22][0]=6;
+                positionWater[22][1]=13;
+
+                positionWater[23][0]=6;
+                positionWater[23][1]=14;
+
+                positionWater[24][0]=6;
+                positionWater[24][1]=15;
+
+
+        for (int i = 0; i < positionWater.length; i++) {
+            gameObjects.add(getTile(tile));
+            gameObjects.get(gameObjects.size-1).setPosition(calculatePosition(positionWater[i][0]), calculatePosition(positionWater[i][1]));
+            System.out.println(positionWater.length);
+        }
+
+
+
+        return gameObjects;
+    }
+
+    public int calculatePosition(int coordinate){
+
+        int position;
+
+        position = -240 + 30 * coordinate;
+
+        return position;
+    }
 
 
 }
